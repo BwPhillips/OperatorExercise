4a3bbe7edccc1e7672bfac93118f099f50c70240
Brenton Phillips <phillips.brenton@gmail.com>
2020-06-22 11:08:44 -0500

    Operator Exercise 1 and 2

---
+++ OperatorExerciseConsole/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace OperatorExerciseConsole
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+
+            // -------------Exercise 1-------------
+
+            var a = 17;
+
+            var b = 4;
+
+            var add = a + b;
+
+            var sub = a - b;
+
+            var mult = a * b;
+             
+            var div = a / b;
+
+            var mod = a % b;
+
+            Console.WriteLine(add);
+
+            Console.WriteLine(sub);
+
+            Console.WriteLine(mult);
+
+            Console.WriteLine($"{a} / {b} is {div} remainder { mod}");
+
+
+            // --------------Exercise 2--------------
+
+            var r = 20;
+            var pi = Math.PI;
+
+            var areaOfCircle = pi * (r * r );
+
+            Console.WriteLine($"The area of the Circle with radius of { r} is {areaOfCircle }");
+        }
+    }
+}
